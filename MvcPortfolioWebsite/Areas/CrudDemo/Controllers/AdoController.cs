7f924b162c09222fbb18f0274c28cf1ed431ae0a
chdtcktt <chdtcktt@gmail.com>
2014-03-09 12:56:58 -0700

    Automapper
    Added viewmodel and automapper back

--- MvcPortfolioWebsite/Areas/CrudDemo/Controllers/AdoController.cs
+++ MvcPortfolioWebsite/Areas/CrudDemo/Controllers/AdoController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using AutoMapper;
+using MvcPortfolioWebsite.Areas.CrudDemo.ViewModels;
 
 namespace MvcPortfolioWebsite.Areas.CrudDemo.Controllers
 {
@@ -17,15 +18,19 @@ namespace MvcPortfolioWebsite.Areas.CrudDemo.Controllers
         public AdoController()
         {
            ado = new CrudOperation();
+            Mapper.CreateMap<Person,PersonViewModel>();
               
         }
 
         public ActionResult Index()
         {
             ViewBag.Header = "CRUD Demo";
-            var data = ado.GetPersons();
 
-            return View(data);
+
+            var data = ado.GetPersons();
+            IEnumerable<PersonViewModel> vm = Mapper.Map<IEnumerable<Person>, IEnumerable<PersonViewModel>>(data);
+
+            return View(vm);
         }
 
 
---
+++ MvcPortfolioWebsite/Areas/CrudDemo/ViewModels/PersonViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcPortfolioWebsite.Areas.CrudDemo.ViewModels
+{
+    public class PersonViewModel
+    {
+        public int PersonId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
